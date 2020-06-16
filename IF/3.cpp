15b28f691b5e48375cf77c1e46adf1d947066e9d
azzyjk <56161518+azzyjk@users.noreply.github.com>
2020-06-16 12:44:39 +0900

    Setup c++

--- IF/3.cpp
+++ IF/3.cpp
@@ -1,20 +1,26 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
+int main()
+{
 	int year;
-	
-	cin>>year;
-	
-	if((year%4)==0){
-		if((year%400)==0) cout<<"1";
-		else if((year%100)!=0) cout<<"1";
-		else cout<<"0";
+
+	cin >> year;
+
+	if ((year % 4) == 0)
+	{
+		if ((year % 400) == 0)
+			cout << "1";
+		else if ((year % 100) != 0)
+			cout << "1";
+		else
+			cout << "0";
 	}
-	else {
-		cout<<"0";
+	else
+	{
+		cout << "0";
 	}
-	
+
 	return 0;
 }
--- IF/4.cpp
+++ IF/4.cpp
@@ -1,23 +1,29 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
+int main()
+{
 	int H, M;
-	
-	cin>>H>>M;
-	
-	if(M>=45){
-		cout<<H<<" "<<M-45;
+
+	cin >> H >> M;
+
+	if (M >= 45)
+	{
+		cout << H << " " << M - 45;
 	}
-	else{
-		if(H==0){
-			cout<<"23"<<" "<<60-(45-M);
+	else
+	{
+		if (H == 0)
+		{
+			cout << "23"
+				 << " " << 60 - (45 - M);
 		}
-		else {
-			cout<<H-1<<" "<<60-(45-M);
+		else
+		{
+			cout << H - 1 << " " << 60 - (45 - M);
 		}
 	}
-	
+
 	return 0;
 }
